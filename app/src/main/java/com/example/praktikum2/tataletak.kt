fea08c1f837706d098b494dc73fffb70920ead10
kiieeaa <kiyaadinda5@gmail.com>
2025-10-16 16:39:47 +0700

    memodif text dalam bentuk box,colim,row,box,column

--- app/src/main/java/com/example/praktikum2/tataletak.kt
+++ app/src/main/java/com/example/praktikum2/tataletak.kt
@@ -44,3 +44,20 @@ fun TataletakRow(modifier: Modifier) {
     }
 
 }
+
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+
+    )   {
+        Text(text = "Box 1")
+        Text(text = "Colim 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+
+    }
+}
