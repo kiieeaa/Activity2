4b74861225b40e5002fdff3e8613ea1f4ae78fe9
kiieeaa <kiyaadinda5@gmail.com>
2025-10-16 17:16:24 +0700

    memodifier pada height 300.dp

--- app/src/main/java/com/example/praktikum2/tataletak.kt
+++ app/src/main/java/com/example/praktikum2/tataletak.kt
@@ -134,9 +134,18 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                     Text(text = "Col1_Row2_Komponen3")
 
                 }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Box(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(height = 300.dp)
+                        .background(color = Color.Cyan),
+                    contentAlignment = Alignment.Center
+                ){
 
 
-            }
+
+                }
         }
 
     }
