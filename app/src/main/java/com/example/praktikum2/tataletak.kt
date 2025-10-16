aa6a3c99be52db89d32e7e0dfb05c84fd2b98a2b
kiieeaa <kiyaadinda5@gmail.com>
2025-10-16 16:54:25 +0700

    modif pada kolom1 pada bagian komponen dan kolom

--- app/src/main/java/com/example/praktikum2/tataletak.kt
+++ app/src/main/java/com/example/praktikum2/tataletak.kt
@@ -83,3 +83,17 @@ fun TataletakColumRow(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataletakRowColumn(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        Column(){
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+
+        }
+
+    }
+
+}
